e86cfddacfd6d09d05de238de8c170954c9592f6
Sammy Wu <swucode@gmail.com>
2018-08-28 02:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send();
 }
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i = 0; i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
