8da622bd3241c1789f9f3b1151aa131fdf1db8bf
Sammy Wu <swucode@gmail.com>
2018-08-28 02:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ function getRepositories() {
 }
 
 function showRepositories(event, data) {
-  console.log(this.responseText)
-  let repoList = "<ul>"
-  for(var i = 0; i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-  }
-  repoList += "</ul>"
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
 }
 
 function getRepositories() {
@@ -21,3 +26,8 @@ function getRepositories() {
   req.send()
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
